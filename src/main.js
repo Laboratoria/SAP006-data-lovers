852e20553bf4c303bef6b280dbfe57c6cdccf26a
Taiene Yuki <taieneyamazaki@gmail.com>
2021-06-21 16:49:46 -0300

    teste

--- src/main.js
+++ src/main.js
@@ -8,20 +8,43 @@ console.log(example, data);
 
 const champs = Object.values(data.data);
 
-function cards (){
-    let i;
-    let champName;
-    let champImg;
-    let card;
-    let name;
+let i;
+let champName;
+let champImg;
+let image;
+let nameConteiner;
+let name;
+let card;
 
-    for (i=0; i<champs.length ; i++){
-        champImg = champs[i].splash;
-        card = document.createElement("DIV");
-        document.getElementById("cards-conteiner").appendChild(card);
-        card.innerHTML = "<img src='"+champImg+"' alt='"+champName+"' class=card>";
-        
-      
-    }    
+for (i = 0; i < champs.length; i++) {
+    champImg = champs[i].splash;
+    champName = champs[i].name;
+    
+    card = document.createElement("div");
+    document.getElementById("cards-conteiner").appendChild(card);
+    card.className = "card";
+    card.setAttribute("id", champName);
+    
+    image = document.createElement("img");
+    image.src = champImg;
+    image.alt = champName+"'s image";
+    image.className = "image";
+    card.appendChild(image);
+
+    nameConteiner = document.createElement("div");
+    name = document.createTextNode(champName);
+    nameConteiner.className = "nameConteiner";
+    nameConteiner.appendChild(name);
+    card.appendChild(nameConteiner);
+ 
 }
-cards();
+
+// function turnCard(item){
+//     if (div.className === "card"){
+//         card = document.createElement("div");
+
+//         })
+//     }
+
+// }
+// turnCard();
