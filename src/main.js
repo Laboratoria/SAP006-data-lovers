7585374891d35b232917f7774c55292263733d38
jenniferpessoa <jenni.pessoa@gmail.com>
2021-06-22 19:40:18 -0300

    Imagem e Texto separados na div Campeão

--- src/main.js
+++ src/main.js
@@ -9,19 +9,28 @@ let champion
 window.addEventListener("load", inicia)
 function inicia() {
 
-  for (champion in dados) {
-    let d = document.createElement("div")
-    //d.class = 'card' --- se precisarmos criar uma classe para a div
-    let texto = document.createTextNode(champion)
+  for (champion in dados) {    
+    let imageCard = document.createElement("img");
+    imageCard = new Image(250, 200); 
+    imageCard.src = `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion}_0.jpg`;
+    /*
+    let c1 = document.querySelector("header")
+    let foto = document.createElement("img");
+    foto.src = dados.Aatrox.splash
+    c1.appendChild(foto);
+    */ 
+        
+    let textCard = document.createElement("p")
+    textCard.class = "t"
+    let nameText = document.createTextNode(champion)
+    textCard.appendChild(nameText)
 
-    //let im = document.querySelector('div');  --- se precisarmos criar uma div para texto 
-    let image = document.createElement("img");
-    image = new Image(200, 150); // Largura (width) e altura (height).
-    image.src = `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion}_0.jpg`;
 
-    d.appendChild(texto)
-    d.appendChild(image);
-    document.body.appendChild(d)   
+    let divCard = document.createElement("div")
+    divCard.appendChild(textCard)
+    divCard.appendChild(imageCard);
+
+    let container = document.querySelector("main")
+    container.appendChild(divCard)   
   } 
 }
-
