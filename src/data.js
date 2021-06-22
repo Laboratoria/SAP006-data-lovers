c59d1ae363859a90633366582094ac356107fcb8
Tauana Pacheco <tauanapachecopp@gmail.com>
2021-06-22 18:26:56 -0300

    refatorando functions

--- src/data.js
+++ src/data.js
@@ -1,25 +1,37 @@
 
-export const filtrar = function (seletorTipos, data) {
-  const pokemon_filtrados = data.pokemon.filter(
+export const filtrar = function (data, seletorTipos) {
+  const pokemon_filtrados = data.filter(
     function (pokemon) {
       return pokemon.type.includes(seletorTipos)
     });
   return pokemon_filtrados;
 }
+// filterData(data, condition): esta função receberia os dados e nos retornaria os que cumprem com a condição.
+
+
 
 export const sortData = (data, sortBy, sortOrder) => {
-  // const orderDatas = sortData.sort(a.pokemon < a.pokemon < b.pokemon ? 1 : -1);
-  // 
-  switch (filtrar) {
-    case data:
-      return pokemon_filtrados.sort(a.generation < b.generation ? 1 : -1)
-    case sortBy:
-      return pokemon_filtrados.sort(a.seletorTipos < b.seletorTipos ? 1 : -1)
-    case sortOrder:
-      return sortData.reverse();
-  }
+  // let someData = 
+
+  //  switch (sortData) {
+  //    case data:
+  //      return POKEMONS
+       
+  //    case sortBy:
+  //      return someData
+  //   case sortOrder:
+    
+    
+  //  }
+
+  
 }
 
+//sortData(data, sortBy, sortOrder): esta função recebe três parâmetros.O primeiro, data,
+ //nos entrega os dados.O segundo, sortBy, diz respeito a qual das informações quer usar para ordenar.O terceiro, 
+//sortOrder, indica se quer ordenar de maneira crescente ou decrescente.
+
+//  .sort(a.generation < b.generation ? 1 : -1)
 // data.sort((a, b) => (a.pokemon > b.pokemon) && (a.pokemon < b.pokemon) ? 1 : -1);
 // sortBy.sort((a, b) => (a.generation > b.generation) ? 1 : -1);
 // sortOrder = sortBy.reverse();
--- src/main.js
+++ src/main.js
@@ -1,33 +1,45 @@
 import { filtrar } from './data.js';
 import { sortData } from './data.js';
-
 import data from './data/pokemon/pokemon.js';
 
 
+let POKEMONS = data.pokemon;
 
-for (let pokemon of data.pokemon) {
-  let imagem = document.createElement("img");
-  imagem.src = pokemon.img
-  document.getElementById("listaPokemon").appendChild(imagem)
-}
 
 let seletorTipos = document.getElementById('tipos');
 
 seletorTipos.addEventListener("change", function () {
   if (seletorTipos.value !== "") {
-    exibir(filtrar(seletorTipos.value, data));
+    POKEMONS = filtrar(data.pokemon, seletorTipos.value)
   }
   else {
-    exibir(data.pokemon)
+    POKEMONS = data.pokemon;
   }
-
+  exibir(POKEMONS)
 });
 
-function exibir(pokemons_filtrados) {
+
+let order = document.getElementById('order');
+
+order.addEventListener("change", function (e) {
+  e.preventDefault();
+  // if (order.value !== "") {
+  (sortData(POKEMONS, order.value));
+   //  let sortBy = POKEMONS
+   //   orderPokemons = Object.value(sortBy)
+  //}
+})
+
+function exibir(itens) {
   document.getElementById("listaPokemon").innerHTML = "";
-  for (let pokemon of pokemons_filtrados) {
+  for (let pokemon of itens) {
     let imagem = document.createElement("img");
     imagem.src = pokemon.img
     document.getElementById("listaPokemon").appendChild(imagem)
   }
 }
+
+exibir(POKEMONS)
+
+
+
