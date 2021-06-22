f59bc6992647e97f9e8d77dbdbd7ed8830dd4551
Lediane <lediane141@gmail.com>
2021-06-22 19:56:35 -0300

    feature/cards

--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
 import { example } from './data.js';
+import { } from './data.js';
 import data from './data/ghibli/ghibli.js';
-console.log(example, data);
+const animations = data.films
+
+//console.log(animations.sort((a,b)=>(a.rt_score > b.rt_score? 1:-1)))
+
+function showPosterFilms(){
+    let photo = "";
+    for(let item of animations) {
+        photo += `<div class = "front-poster">
+        <p class="title-of-film"> <strong>${item.title}</strong></p>
+        <img src="${item.poster}" class="poster-card">
+        </div>`
+    } 
+    //return photo
+    document.getElementById("poster-cards").innerHTML = photo;
+}
+console.log(showPosterFilms())
