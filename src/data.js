91d5fc4509a3ffaf4460839eaba56d892657ec7e
Aline Andrade <marson.aline@gmail.com>
2021-06-21 22:10:47 -0300

    trabalho parcial

--- src/data.js
+++ src/data.js
@@ -1,9 +1,16 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+//export const example = () => {
+  //return 'example';
+//};
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+//export const anotherExample = () => {
+  //return 'OMG';
+//};
+
+export filtrar from './main.js';
+
+function filtrar (item) {
+  const pokemon_filtrados = data.pokemon.filter(function(item) {
+  return item.type.includes("water")
+});
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import { example } from './data.js';
+import filtrar from './data.js';
 
 import data from './data/pokemon/pokemon.js';
 
 
-for(let pokemon of data.pokemon){
-    let imagem = document.createElement("img");
-    imagem.src = pokemon.img
-    document.getElementById("listaPokemon").appendChild(imagem)
-  }
+for (let pokemon of data.pokemon) {
+  let imagem = document.createElement("img");
+  imagem.src = pokemon.img
+  document.getElementById("listaPokemon").appendChild(imagem)
+}
+
+let btn = document.querySelector('button');
+btn.addEventListener("click", filtrar);
+
+
