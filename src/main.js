f26bed452cb820d399235054e58644084eb91409
Larissa Siqueira <larissaraie@gmail.com>
2021-06-23 20:58:39 -0300

    cards prontos

--- src/main.js
+++ src/main.js
@@ -21,12 +21,28 @@ for (let i = 0; i < objects.length; i++) {
     divBackCard.setAttribute("class", "backcard");
     divCards.appendChild(divBackCard);
 
+    let imageCharacters = document.createElement("img");
+    imageCharacters.setAttribute("id", "imageCharacters");
+
     const name = objects[i].name;
     const title = objects[i].title;
     const splash = objects[i].splash;
     const blurb = objects[i].blurb;
+    
+    divFrontCard.innerHTML = `<h1>${name}</h1><h2>${title}</h2><img id="imageCharacters" src="${splash}"><p>${blurb}</p>`;
 
-    divFrontCard.innerHTML = `<h1>${name}</h1><h2>${title}</h2><img src="${splash}"><p>${blurb}</p>`;
+    const img = objects[i].img;
+    const partype = objects[i].partype;
+    const tags = objects[i].tags;
+    const attack = objects[i].info.attack;
+    const defense = objects[i].info.defense;
+    const magic = objects[i].info.magic;
+    const difficulty = objects[i].info.magic;
+
+    divBackCard.innerHTML = `<h3>${name}</h3><img src="${img}"><h4>${tags}</h4><h4>${partype}</h4>
+    <br><p id="attack">Attack: ${attack}</p><p id="defense">Defense: ${defense}</p><p id="magic">Magic: ${magic}</p>
+    <p id="difficulty">Difficulty: ${difficulty}</p>"`
+    
 }
 
 // console.log(example, data); 
