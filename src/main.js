a97a16e70b96553e7a5d7940c61586622cdb46ed
jenniferpessoa <jenni.pessoa@gmail.com>
2021-06-24 10:58:32 -0300

    Cards em CardPack - Função Aperfeiçada

--- src/main.js
+++ src/main.js
@@ -1,36 +1,31 @@
-import { example } from './data.js';
 import data from './data/lol/lol.js';
 
 // Campeão aparece na tela
+let dataLol = data.data1
 
-let dados = data.data1
-let champion
+ListingCards(dataLol)
 
-window.addEventListener("load", inicia)
-function inicia() {
+function ListingCards(itens) {
+  let cardPack = document.querySelector("ul.cards")
+  //cardPack.innerHTML = ""  -- novas funções precisarão disso para limpar a tela  
 
-  for (champion in dados) {    
+  for (let champion in itens) {
+    const info = dataLol[champion]
+
     let imageCard = document.createElement("img");
-    imageCard = new Image(250, 200); 
-    imageCard.src = `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion}_0.jpg`;
-    /*
-    let c1 = document.querySelector("header")
-    let foto = document.createElement("img");
-    foto.src = dados.Aatrox.splash
-    c1.appendChild(foto);
-    */ 
-        
-    let textCard = document.createElement("p")
-    textCard.class = "t"
+    imageCard = new Image(250, 200); // preciso fazer isso no css? 
+    imageCard.src = info.splash
+
+
+    let textCard = document.createElement("p") // textCard.class = "t"
     let nameText = document.createTextNode(champion)
     textCard.appendChild(nameText)
 
 
-    let divCard = document.createElement("div")
-    divCard.appendChild(textCard)
-    divCard.appendChild(imageCard);
+    let card = document.createElement("li")
+    card.appendChild(textCard)
+    card.appendChild(imageCard);
 
-    let container = document.querySelector("main")
-    container.appendChild(divCard)   
-  } 
+    cardPack.appendChild(card)
+  }
 }
