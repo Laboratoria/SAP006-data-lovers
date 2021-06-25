eacb7c8f8dd2d072247d527693ca4259669e4e92
Joice Souza <joice_fsouza@hotmail.com>
2021-06-25 16:55:15 -0300

    cards na tela

--- src/data.js
+++ src/data.js
@@ -1,9 +1,24 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
+
+
+
+//export const studioGlibli = (films, elements) => {
+  
+
+  
+  
+   //usado para pegar o valor de cada item
+
+  //const filterFilms = films.filter
+  //return 'example';
+//};
+
+/*export const example = () => {
   return 'example';
 };
 
 export const anotherExample = () => {
   return 'OMG';
-};
+};*/
+
