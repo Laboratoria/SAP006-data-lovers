cf9f414b1608d58acda132fbc3d386985e28f114
Gabrielly Mendes <silvamgabi@hotmail.com>
2021-06-25 18:31:17 -0300

    cards sendo filtrados por status e gender

--- src/data.js
+++ src/data.js
@@ -1,14 +1,29 @@
-// estas funciones son de ejemplo
-
-export const filterData = (data, condicao) => {
-  const filterResults = data.results.filter(results => results.status === condicao || results.gender === condicao);
-  console.log(filterResults);
-  if (filterResults == "") {
+export const filterData = (data, condicao, condicao2) => {
+  
+  
+  if(condicao !== "" && condicao2 !== ""){
+    const filterResults = data.results.filter(results => results.status === condicao && results.gender === condicao2);
+    return filterResults;
+    }
+  
+  else if(condicao2 !== ""){
+    const filterResults = data.results.filter(results => results.gender === condicao2);
+    return filterResults;
+  }
+  
+  else if(condicao !== ""){
+    const filterResults = data.results.filter(results => results.status === condicao);
+    return filterResults;
+  }
+  
+  
+  //console.log(filterResults);
+  /*if (filterResults == "") {
     throw TypeError("Valor de filtro vazio")
   }
   else {
     return filterResults;
-  }
+  }*/
 };
 
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,42 @@
 import { filterData } from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
-//console.log(example, data);
-
 const btn = document.getElementById('btn');
 btn.addEventListener("click", (e) => filtrar(e));
 
 const cards = document.querySelector('.cards');
 const staticCards = data.results.map(({ name, status, gender, image }) =>
+  `<div class="cards_container">
+        <div class="character_img">
+          <img src="${image}">
+        </div>
+         <div class="character_info">
+          <span id="character_name">
+            <h3>${name}</h3>
+          </span>
+          <span id="character_status">${status} - ${gender}</span>
+          <span id="episodes">Episódios</span>
+            <p>1, 2, 3, 4,</p>
+         </div>
+     </div>`).join("")
+
+cards.innerHTML += staticCards;
+
+
+// Função filtrar:    
+
+const filtrar = function (e) {
+  e.preventDefault();
+
+  const statusFilter = document.getElementById("status-filter")
+  const statusOptions = statusFilter.options[statusFilter.selectedIndex].value
+
+  const genderFilter = document.getElementById("gender-filter")
+  const genderOptions = genderFilter.options[genderFilter.selectedIndex].value
+
+  const valor_filtrado = filterData(data, statusOptions, genderOptions);
+
+  const printCards = valor_filtrado.map(({ name, status, gender, image }) =>
     `<div class="cards_container">
         <div class="character_img">
           <img src="${image}">
@@ -22,54 +51,6 @@ const staticCards = data.results.map(({ name, status, gender, image }) =>
          </div>
      </div>`).join("")
 
-     cards.innerHTML += staticCards;
-
-
- // Função filtrar:    
-
-const filtrar = function(e){
-e.preventDefault();
-
-const filtro = document.getElementById("status-filter")
-const options = filtro.options[filtro.selectedIndex].value
-
-const valor_filtrado = filterData(data, options);
-
-const printCards = valor_filtrado.map(({ name, status, gender, image }) =>
-    `<div class="cards_container">
-        <div class="character_img">
-          <img src="${image}">
-        </div>
-         <div class="character_info">
-          <span id="character_name">
-            <h3>${name}</h3>
-          </span>
-          <span id="character_status">${status} - ${gender}</span>
-          <span id="episodes">Episódios</span>
-            <p>1, 2, 3, 4,</p>
-         </div>
-     </div>`).join("")
-
-cards.innerHTML = "";
-cards.innerHTML += printCards
-//console.log(printCards)
+  cards.innerHTML = "";
+  cards.innerHTML += printCards
 }
-
-
-
-
-
-
-
-
-
-
-
-
-//const filtro2 = document.getElementById("filtro2")
-
-//const options2 = filtro2.options[filtro2.selectedIndex].value
-
-
-
-
