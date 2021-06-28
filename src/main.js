3d63ad46e221d1934a42d249bd5e26a58221845b
Tauana Pacheco <tauanapachecopp@gmail.com>
2021-06-28 14:10:52 -0300

    ajustes na ordenação

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
-import { filtrar } from './data.js';
-import { sortData } from './data.js';
+import { filtrar, sortData, media } from './data.js';
 import data from './data/pokemon/pokemon.js';
 
 
 let POKEMONS = data.pokemon;
+
+
+function exibir(itens) {
+  document.getElementById("listaPokemon").innerHTML = "";
+  for (let pokemon of itens) {
+    let imagem = document.createElement("img");
+    imagem.src = pokemon.img
+    document.getElementById("listaPokemon").appendChild(imagem)
+  }
+}
+exibir(POKEMONS)
 
 
 const type = document.getElementById('type');
@@ -11,40 +21,25 @@ const type = document.getElementById('type');
 type.addEventListener("change", function () {
   if (type.value !== "") {
     POKEMONS = filtrar(data.pokemon, type.value)
-    exibir(POKEMONS)
-}})
-
-
-  seletorTipos.addEventListener("change", function () {
-    if (seletorTipos.value !== "") {
-      POKEMONS = filtrar(data.pokemon, seletorTipos.value)
-    }
-    else {
-      POKEMONS = data.pokemon;
-    }
-    exibir(POKEMONS)
-  });
-
-
-  const order = document.getElementById('order');
-
-  order.addEventListener("change", function (e) {
-    e.preventDefault();
-
-    const orderValues = order.value.split("/");
-    const sortResults = sortData(POKEMONS, orderValues[0], orderValues[1]);
-
-    exibir(sortResults);
-  })
-
-  function exibir(itens) {
-    document.getElementById("listaPokemon").innerHTML = "";
-    for (let pokemon of itens) {
-      let imagem = document.createElement("img");
-      imagem.src = pokemon.img
-      document.getElementById("listaPokemon").appendChild(imagem)
-    }
   }
+  else {
+    POKEMONS = data.pokemon;
+  }
+  exibir(POKEMONS)
+});
+
+
+const order = document.getElementById('order');
+
+order.addEventListener("change", function (e) {
+  e.preventDefault();
+
+  const orderValues = order.value.split("/");
+  const sortnames = sortData(POKEMONS, orderValues[0], orderValues[1]);
+  exibir(sortnames);
+  
+})
+
 
 
 
