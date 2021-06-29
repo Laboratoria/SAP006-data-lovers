a06a76a805676fd33e349d1a43c50b702e31bf5d
Carmen Emilia Gozza <carmemilya1@gmail.com>
2021-06-29 17:25:30 -0300

    consertando conflitos

--- src/main.js
+++ src/main.js
@@ -2,10 +2,35 @@ import { filterData } from './data.js';
 
 import data from './data/rickandmorty/rickandmorty.js';
 
+var initialData = data.results;
+
+var filteredData = data.results;
+
+
+function showCards(data) {
+    document.getElementById('get-cards').innerHTML = data.map((item) => `
+        <div class="info-cards">
+        <div class="img-cards">
+        <img src="${item.image}">
+        </div>
+            <ul class="list">
+                <li>Nome:${item.name}</li>
+                <li>Status:${item.status}</li>
+                <li>Espécie:${item.species}</li>
+                <li>Origem:${item.origin.name}</li>
+            </ul>
+        </div>
+        
+        `)
+}
+showCards(data.results);
+
+
 
 
 const status = document.getElementById("status");
 const species= document.getElementById("especie");
+
 function getStatus(statusSelected) {
 
     var test = data.results;
