656becc5479daa3f72d5ac4b92aaec00362f1878
Letícia Braga Aniceto <leticia.aniceto@usp.br>
2021-06-30 19:38:10 -0300

    cards na tela e WIP data.js

--- src/data.js
+++ src/data.js
@@ -1,19 +1,12 @@
-// estas funciones son de ejemplo
+export const filters = (data, condition) => {
+  
+  filterDirector: function directors (name) {
+  if (condition !== ""){
+    let filterResult = "";
+    for (let i = 0; i < name.length; i++){
+      let 
+    }
+    filterDirector += filterResult;
+  } 
+}
 
-/*export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};*/
-
-import { doc } from "prettier";
-import ghibli from "./data/ghibli/ghibli";
-
-/*function filterWhoDid (){
-  let input, filter, ul, il, a, i;
-  input = document.getElementById("");
-  filter = document.getElementById("");
-  ul = 
-}*/
--- src/main.js
+++ src/main.js
@@ -1,27 +1,39 @@
-import { director, producer } from './data.js';
+import { filterDirector } from './src/data.js';
 import data from './data/ghibli/ghibli.js';
+const films = data.films;
+// console.log(films);
 
-const selectFilterDirector = document.querySelector("#director");
-const selectFilterProducer = document.querySelector("#producer");
+const cards = document.querySelector(".mainCards");
 
-showingCards(data.ghibli);
-
-function showingCards(ghibliCards) {
-    let showCards = document.querySelector("#main-cards");
-    let card = "";
-    for () {
-        let director = "";
-        if () {
-
-        }
-    }
-
+for (const film of films) {
+    const title = film.title;
+    const poster = film.poster;
+    const elementTitle = `
+    <div class="film">
+        <img src="${poster}" />
+        <p class="title">${title}</p>
+    </div>`;
+    // console.log(elementTitle);
+    cards.innerHTML += elementTitle;
 }
 
+const selectDirector = document.getElementById("director");
+selectDirector.addEventListener("change", (event) => {
+    const selectedDirector = event.target.value;
+    console.log(selectedDirector);
+    cards.innerHTML = "";
+});
 
+/*
+films.map(function(filmeAtual, indice, array) {
+    // console.log("filmeAtual:", filmeAtual)
+    // console.log("indice:", indice)
+    // console.log("array:", array)
+})
 
-
-
-console.log(example, data);
-
+for (let i = 0; i < films.length; i++) {
+    const element = films[i];
+    console.log(i, element)
+}
+*/
 
