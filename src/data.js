d5377cf59b2b4a5ddcbaa0dca868ddd10abd0a39
KamilaMoura1 <kamila.demoura93@gmail.com>
2021-06-30 13:06:54 -0400

    Atualizações JS, HTML e CSS

--- src/data.js
+++ src/data.js
@@ -1,9 +1,27 @@
-// estas funciones son de ejemplo
+export const deadOrAlive = (data, condicao1, condicao2, condicao3, condicao4) => {
+    let filtros = '',
 
-export const example = () => {
-  return 'example';
-};
+    if (condicao1 === '' && condicao2 === '') {
+        throw TypeError(alert('Insira um filtro válido'));
 
-export const anotherExample = () => {
-  return 'OMG';
+    } else if (condicao1 !== '' && condicao2 !== '' && condicao3 !== '' && condicao4 !== '') {
+        filtros = data.results.filter(results => results.status === condicao1 && results.species
+            === condicao2);
+        return filtros;
+
+    } else if (condicao1 !== '') {
+        filtros = data.results.filter(results => results.status === condicao1);
+        return filtros
+
+    } else if (condicao2 !== '') {
+        filtros = data.results.filter(results => results.species === condicao2); return filtros
+
+    } else if (condicao3 !== '') {
+        filtros = data.results.filter(results => results.type === condicao3);
+        return filtros
+
+    } else if (condicao4 !== '') {
+        filtros = data.results.filter(results => results.origin === condicao4);
+        return filtros
+    }
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,47 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+import { deadOrAlive } from './data.js';
+import data from './data/rickandmorty/rickandmorty.js';;
+
+botaoPesquisar.getElementById('buscar');
+botao.addEventListener('click', (e) => filtrar(e));
+
+ const cards = document.querySelector.map('.cards');
+ const cartoes = data.results.map(({ status, species, type, origin }) =>
+     `<div id="container-card">
+     <img src="${image}" id="foto-card"></img>
+     </div>
+
+ <div>
+
+   <span id="type">
+     <h3>${type}</h3>
+   </span>
+      
+   <span id="species">
+     <p>${species}</p>
+   </span>
+
+   <span id="status">
+     <p>${status}</p>
+   </span>
+
+ </div>`).join("");
+
+ cards.innerHTML += cartoes;
+
+ const filtrar = function (k) {
+     k.preventDefault();
+
+     const filtroStatus = document.getElementById('caracter-status')
+     const opcoesStatus = status.filter.option[filtroStatus.seletor].value;
+
+     const filtroEspecie = document.getElementById('caracter-species')
+     const opcoesEspecie = species.filer.option[filtroEspecie.seletor].value;
+
+     const filtroTipo = document.getElementById('caracter-type')
+     const opcoesTipo = type.filer.option[filtroTipo.seletor].value;
+
+     const filtroOrigem = document.getElementById('caracter-filter')
+     const opcoesOrigem = origin.filer.option[filtroOrigem.seletor].value;
+ }   
+
