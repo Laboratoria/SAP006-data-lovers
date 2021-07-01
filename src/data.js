cef6c2374597bbfbe2fd1ccfaa6eb7ac95cfbebd
Your Name <you@example.com>
2021-07-01 18:33:23 -0300

    feature lizi com alterações

--- src/data.js
+++ src/data.js
@@ -1,9 +1,21 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+export const filtrar = function (data, selecionarFiltro) {
+  const pokemonsFiltrados = data.filter(function (pokemon) {
+    return pokemon.type.includes(selecionarFiltro);
+  });
+  return pokemonsFiltrados;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+
+
+
+/*filterData(data, condition): esta função receberia os dados e nos retornaria os que cumprem com a condição.*/
+
+
+
+/*
+sortData(data, sortBy, sortOrder): esta função recebe três parâmetros. O primeiro, data, nos entrega os dados. O segundo, sortBy, diz respeito a qual das informações quer usar para ordenar. O terceiro, sortOrder, indica se quer ordenar de maneira crescente ou decrescente.
+export const ordenar = function (data, ...) {
+  const todospokemons = alguma coisa
+    return lalalala;
+  }
+*/
--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import { filtrar } from "./data.js";
+/*import { ordenar } from "./data.js";*/
+import data from "./data/pokemon/pokemon.js";
 
-console.log(example, data);
+let AllPokemons = data.pokemon;
+const filtroTipos = document.getElementById("type");
+const botaoParaFiltrar = document.getElementById("btnFiltrar");
+
+botaoParaFiltrar.addEventListener("click", apertandoBotao);
+
+function apertandoBotao() {
+  if (filtroTipos.value !== "") {
+    AllPokemons = filtrar(data.pokemon, filtroTipos.value);
+  } else {
+    AllPokemons = data.pokemon;
+  }
+  return AllPokemons;
+}
+
+for (let pokemon of AllPokemons) {
+  document.getElementById("type").innerHTML += `${pokemon.name}
+`;
+}
