b2bf4cb2295aa93d69cebc88be843e1d6980a67e
belobruna <bruna.belo@gmail.com>
2021-07-01 09:51:45 -0300

    pagina film.html e cards.css

--- src/data.js
+++ src/data.js
@@ -1,9 +1,18 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+// export const example = () => {
+//   return 'example';
+// };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
+
+
+export function showFilms(film) {
+  film.map((name) => console.log(name.title))
+}
+
+export function showDescripton(sinopse) {
+  sinopse.map((name) => console.log(name.description))
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import {showFilms, showDescripton} from './data.js';
+import data from './data/ghibli/ghibli.js';
 
-console.log(example, data);
+console.log(showFilms(data.films));
+console.log(showDescripton(data.films));
