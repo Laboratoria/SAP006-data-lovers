50e229a951e7eff8af2ae1dc88a7fbb3b5ad5a93
Your Name <you@example.com>
2021-07-02 16:41:27 -0300

    resultados printados, filtro não funciona

--- src/data.js
+++ src/data.js
@@ -1,21 +1,6 @@
-export const filtrar = function (data, selecionarFiltro) {
-  const pokemonsFiltrados = data.filter(function (pokemon) {
-    return pokemon.type.includes(selecionarFiltro);
-  });
-  return pokemonsFiltrados;
+export const showPokemons = (pokemonsList) => pokemonsList.slice(0, 20);
+
+export const filterByName = (pokemonsList, pokemonsName) => {
+  const lowerCaseName = pokemonsName.toLowerCase();
+  return pokemonsList.filter(pokemon => pokemon.name.toLowerCase().startWith(lowerCaseName)).slice(0, 20);
 };
-
-
-
-
-/*filterData(data, condition): esta função receberia os dados e nos retornaria os que cumprem com a condição.*/
-
-
-
-/*
-sortData(data, sortBy, sortOrder): esta função recebe três parâmetros. O primeiro, data, nos entrega os dados. O segundo, sortBy, diz respeito a qual das informações quer usar para ordenar. O terceiro, sortOrder, indica se quer ordenar de maneira crescente ou decrescente.
-export const ordenar = function (data, ...) {
-  const todospokemons = alguma coisa
-    return lalalala;
-  }
-*/
