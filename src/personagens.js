0b95f8e63e4f12229029859edaf020431f9ec2ed
Lediane <lediane141@gmail.com>
2021-07-02 12:21:17 -0300

    teste

--- src/personagens.js
+++ src/personagens.js
@@ -1,10 +1,6 @@
 
 import { filterSpeciesSelected, filterGender, order} from './data.js';
 import data from './data/ghibli/ghibli.js';
-import { filterSpeciesSelected, order} from './data.js';
-import { } from './data.js';
-import data from './data/ghibli/ghibli.js';
-const animations = data.films
 const characters = data.films.reduce(function(chars, film){ 
             
     // [...chars, ...film.people]
