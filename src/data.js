6320045fd31c0d2fa70a560d2175c83d4ea90301
larissamaranho <larissamaranhor@gmail.com>
2021-07-02 19:35:27 -0300

    Cards filmes na tela

--- src/data.js
+++ src/data.js
@@ -7,10 +7,17 @@
 // };
 
 
-export function showFilms(film) {
-  film.map((name) => console.log(name.title))
-}
+ export function showFilms(film) {
+   film.map((name) => console.log(name.title))
+  }
+   
 
-export function showDescripton(sinopse) {
-  sinopse.map((name) => console.log(name.description))
-}
+
+// filterData(data, condition): esta função receberia os dados e nos retornaria os que cumprem com a condição.
+
+// sortData(data, sortBy, sortOrder): esta função recebe três parâmetros. O primeiro, data, nos entrega os dados. 
+  //O segundo, sortBy, diz respeito a qual das informações quer usar para ordenar. O terceiro, sortOrder, 
+  //indica se quer ordenar de maneira crescente ou decrescente.
+
+// computeStats(data): essa função nos permite fazer cálculos estatísticos básicos para serem exibidos de acordo 
+  //com o que os dados permitem.
--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
-import {showFilms} from './data.js';
+import { showFilms } from './data.js';
 import data from './data/ghibli/ghibli.js';
-console.log(showFilms(data.films));
 
+console.log(showFilms(data.films))
+
+//função para criar cards dos filmes
+//const listaFilmes = showFilms(data.ghibli)
+
+const printCardsFilms = (listaFilmes) => {
+    listaFilmes.forEach(film => {
+        const cardElements = document.getElementById("card");
+        cardElements.innerHTML += `<div class="cartaoFilme">
+            <h2 class="title"> ${film.title} </h2>
+            <img class="poster" src= ${film.poster}>
+            </div>`
+    })
+}
+
+    const botaoFilmes = document.getElementById("animation")
+
+    botaoFilmes.addEventListener("click", () => {
+        const listaFilmes = showFilms(data.ghibli)
+        printCardsFilms(listaFilmes)
+    })
+
+printCardsFilms(data.films)
