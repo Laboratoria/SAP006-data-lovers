089fe08a00b6efd0dd8c8b0dd31f67316a93a5d0
bruna-devbio <bruna.ppp8@gmail.com>
2021-07-02 15:42:16 -0300

    Funçao para pesquisar por personagem

--- src/data.js
+++ src/data.js
@@ -1,24 +1,3 @@
-//Funções para ordenar;
 
+//Funções para filtrar;
 
-
- //Funções para filtrar;
-
-export const filterData = (data, condition) => {
-  const filterOn = data.results.filter(results => results.name === condition || results.status === condition ||
-  results.species === condition || results.origin.name);
-  if (filterGo == "") {
-    throw TypeError("Selecione um filtro")
-  }
-  else{
-    return filterOn;
-  }
-}
-
-//Funções para cálculo agregado;
-
-
-/*
-export const anotherExample = () => {
-  return 'OMG';
-*/
--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-import { filterData } from './data.js';
+import {getStatus, getSpecies} from './data.js';
+
 import data from './data/rickandmorty/rickandmorty.js';
 
-
+// 1) Exibir cards na tela
 function showCards(data) {
     document.getElementById('get-cards').innerHTML = data.map((item) => `
-        <div class="info-cards">
-        <div class="img-cards">
+        <div class ="info-cards">
+        <div class ="img-cards">
         <img src="${item.image}">
         </div>
-            <ul class="list">
-                <li>Nome:${item.name}</li>
+        <div class ="list-cards">
+            <ul class ="list">
+                <li id="name">Nome:${item.name}</li>
                 <li>Status:${item.status}</li>
                 <li>Espécie:${item.species}</li>
                 <li>Origem:${item.origin.name}</li>
@@ -19,3 +21,61 @@ function showCards(data) {
         `)
 }
 showCards(data.results);
+
+
+// 2) Função para pesquisar por nome de personagem:
+const btnSearch = document.getElementById("button-search");
+
+function searchCharacter(e) {
+   e.preventDefault()
+
+   let searchTerm = document.getElementById("characters").value;
+   console.log(searchTerm);
+   let object = data.results;
+   object = object.filter(character => character.name === searchTerm);
+   showCards(object);
+}
+
+btnSearch.addEventListener('click', searchCharacter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+formSearch.addEventListener("submit", event => {
+    event.preventDefault()
+
+    const searchTerm = name.value.trim();
+    console.log(searchTerm);
+
+    if (typeof searchTerm == "number" || typeof searchTerm !== "string") {
+        throw TypeError("Por favor, digite um termo válido")
+        return
+    }
+
+    function foundCharacter(data, searchTerm) {
+        const object = data.results;
+        console.log(object);
+        object = object.filter(character => character.name.toUpperCase === searchTerm.toUpperCase)
+        showCards(object);
+        foundCharacter()
+    }
+})
+*/
