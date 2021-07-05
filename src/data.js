327afa077f22bdf6323235f537ec77eb38dba400
ericaserpa <ericaserpa@hotmail.com>
2021-07-05 17:58:13 -0300

    Ordenação A-Z

--- src/data.js
+++ src/data.js
@@ -1,9 +1,22 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
+/*export const example = () => {
   return 'example';
 };
 
 export const anotherExample = () => {
   return 'OMG';
+};*/
+
+
+
+export const ordemAlfabetica = (data, order) => {
+  if (order === "crescente") {
+    return data.sort((a, z) => a.title > z.title ? 1 : -1)
+  } else if (order === "decrescente") {
+    return data.sort((a, z) => a.title > z.title ? -1 : 1)
+  } return data
 };
+
+
+
