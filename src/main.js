435e4a77900e006b8ba6bb1eee5f7007e6f06f32
ericaserpa <ericaserpa@hotmail.com>
2021-07-05 17:09:10 -0300

    Ordenação A-Z

--- src/main.js
+++ src/main.js
@@ -1,20 +1,25 @@
 //import { example } from './data.js';
 //console.log(example, data);
 
+//import { dataghibli } from './data.js';
+
+import { ordemAlfabetica } from './data.js';
+
 import data from './data/ghibli/ghibli.js';
 
-/////////////////// Importar Filmes ////////////////////
+// Importar Filmes//
 
 const films = data.films
 
 const listaImpressa = document.getElementById("listaImpressa")
 
-const showFilme = (filmList) => {
+const showFilmes = (filmList) => { 
+  listaImpressa.innerHTML = ""
   filmList.forEach(film => {
     listaImpressa.innerHTML += `  
     <li class="cardGhibli"> 
     <img class="card-poster" src=" ${film.poster}">
-    <p class="card-titulo"> Title: ${film.title} </p> 
+    <p class="card-titulo"> ${film.title} </p> 
     <p class="card-lançamento"> Release date: ${film.release_date} </p>
     <p class="card-descrição"> Description: ${film.description} </p>    
     <p class="card-diretor"> Director: ${film.director} </p>
@@ -25,19 +30,19 @@ const showFilme = (filmList) => {
   })
 
 }
-showFilme(films)
+showFilmes(films)
 
 
-  ///////////////////Importar Personagem ////////////////
+//Importar Personagem //
 
 const listaPersonagem = document.getElementById("listaPersonagem")
-const showPeople = (peopleList, film)=>{
-  peopleList.forEach(people=> {
-    listaPersonagem.innerHTML += 
-    `
+const showPeople = (peopleList, film) => {
+  peopleList.forEach(people => {
+    listaPersonagem.innerHTML +=
+      `
     <li class="cardPersonagem">
-          <img src=${people.img} class="card-img" alt="ImagemPersonagem" >
-          <p class="card-nome"> Personagem: ${people.name}</p>
+          <img class="card-poster" src=" ${people.img}">
+          <p class="card-personagens"> Personagem: ${people.name}</p>
           <p class="card-filme"> Filme: ${film} </p>
           <p class="card-gênero"> Genero: ${people.gender}</p>
           <p class="card-idade"> Idade: ${people.age}</p>
@@ -51,3 +56,14 @@ for (let film of films) {
   let peopleList = film.people
   showPeople(peopleList, film.title)
 }
+
+//ORDENAR //
+
+
+const ordenar = document.querySelector(".order");
+function ordemFilme(event) {
+  const order = ordemAlfabetica(data.films, event.target.value)
+  console.log(order)
+  showFilmes(order);
+}
+ordenar.addEventListener("change", ordemFilme);
