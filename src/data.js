7cd1ad68b253038cf747826cc4b0eb329b49b095
KamilaMoura1 <kamila.demoura93@gmail.com>
2021-07-06 15:22:56 -0400

    Atualizações cards

--- src/data.js
+++ src/data.js
@@ -1,27 +1,13 @@
-export const deadOrAlive = (data, condicao1, condicao2, condicao3, condicao4) => {
-    let filtros = '',
+export const example = () => {
+    return 'example';
+  };
+  // export const filtro = () => {
+//   };
 
-    if (condicao1 === '' && condicao2 === '') {
-        throw TypeError(alert('Insira um filtro válido'));
+// personagens.sort(function (a, b) {
+	
+// 	return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);
+ 
+// });
 
-    } else if (condicao1 !== '' && condicao2 !== '' && condicao3 !== '' && condicao4 !== '') {
-        filtros = data.results.filter(results => results.status === condicao1 && results.species
-            === condicao2);
-        return filtros;
 
-    } else if (condicao1 !== '') {
-        filtros = data.results.filter(results => results.status === condicao1);
-        return filtros
-
-    } else if (condicao2 !== '') {
-        filtros = data.results.filter(results => results.species === condicao2); return filtros
-
-    } else if (condicao3 !== '') {
-        filtros = data.results.filter(results => results.type === condicao3);
-        return filtros
-
-    } else if (condicao4 !== '') {
-        filtros = data.results.filter(results => results.origin === condicao4);
-        return filtros
-    }
-};
--- src/main.js
+++ src/main.js
@@ -1,47 +1,74 @@
+// import { filtro } from './data.js';
+import data from "./data/rickandmorty/rickandmorty.js";
 
-import { deadOrAlive } from './data.js';
-import data from './data/rickandmorty/rickandmorty.js';;
+const personagens = data.results;
+// console.log(data.results[0]);
 
-botaoPesquisar.getElementById('buscar');
-botao.addEventListener('click', (e) => filtrar(e));
+const botao = document.getElementById("buscar");
+botao.addEventListener("click", (e) => filtrar(e));
 
- const cards = document.querySelector.map('.cards');
- const cartoes = data.results.map(({ status, species, type, origin }) =>
-     `<div id="container-card">
-     <img src="${image}" id="foto-card"></img>
-     </div>
+function mostrarCartoes(itens) {
+  const cards = document.querySelector("#resultado");
+  cards.innerHTML = "";
+  //   if (mostrarCartoes === []) {
+  //     return alert('Filtro não encontrado')
+  //   }
+  const cartoes = itens.map(({ status, species, type, origin, image, name }) =>
+        ` <section class="result-card" id="resultado">
+        <div class="flip">
+          <div class="card">
+            <div class="front-card">
+              <div><img  class="image" src="${image}" alt="personagens Rick and Morty"></div>
+              <h3 class="text">${name}</h3>
+            </div>
 
- <div>
+            <div class="back-card">
+              <h3 class="text-back-card">Status</h3>
+              <p class="legend">${status}</p>
 
-   <span id="type">
-     <h3>${type}</h3>
-   </span>
-      
-   <span id="species">
-     <p>${species}</p>
-   </span>
+              <h3 class="text-back-card">Gênero</h3>
+              <p class="legend">${type}</p>
 
-   <span id="status">
-     <p>${status}</p>
-   </span>
+              <h3 class="text-back-card">Espécie</h3> 
+              <p class="legend">${species}</p> 
+            </div>
+          </div>
+        </div>
+      </section> `
+    )
+    .join("");
 
- </div>`).join("");
+  document.getElementById("resultado").innerHTML += cartoes;
+}
 
- cards.innerHTML += cartoes;
+const filtrar = function (k) {
+  k.preventDefault();
 
- const filtrar = function (k) {
-     k.preventDefault();
+  //   console.log('clicou')
 
-     const filtroStatus = document.getElementById('caracter-status')
-     const opcoesStatus = status.filter.option[filtroStatus.seletor].value;
+  const filtroStatus = document.getElementById("caracter-status").value;
+  const filtroSpecies = document.getElementById("caracter-species").value;
+  const filtoGender = document.getElementById("caracter-gender").value;
+  const ordenaraz = document.getElementById("ordenar").value;
+  
+//   const ordem = function(a,b){
+//     ordenaraz.sort(function (a, b) {
+//         return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);
+     
+//      });
+//     console.log(ordenaraz)
+//   }
 
-     const filtroEspecie = document.getElementById('caracter-species')
-     const opcoesEspecie = species.filer.option[filtroEspecie.seletor].value;
+  const personagensFiltrados = personagens.filter(function (personagem) {
+    if (
+      personagem.status === filtroStatus ||
+      personagem.species === filtroSpecies ||
+      personagem.gender === filtoGender 
+    ) {
+      return personagem;
+    }
+  });
+  mostrarCartoes(personagensFiltrados);
+};
 
-     const filtroTipo = document.getElementById('caracter-type')
-     const opcoesTipo = type.filer.option[filtroTipo.seletor].value;
-
-     const filtroOrigem = document.getElementById('caracter-filter')
-     const opcoesOrigem = origin.filer.option[filtroOrigem.seletor].value;
- }   
-
+personagensFiltrados.slice([0[9]]);
