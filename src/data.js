2134d1fd3b2fc92c4693b7de4d25c48c6a9c3f0f
Paloma Queiroz <palomacqueiroz@hotmail.com>
2021-07-06 10:49:29 -0300

    inclusao funcao ordenar-filmes

--- src/data.js
+++ src/data.js
@@ -5,6 +5,27 @@ export const filterData = (data, type, condition) => data.filter((element) => {
   return element[type] === condition;
 });
 
+export const sortMovies = (data, condition) => data.sort((a,b) => {
+  const x = a.title.toUpperCase();
+  const y = b.title.toUpperCase();
+  if(condition === "alphabetic"){
+    if(x === y){
+      return 0
+    } else if (x < y){
+      return -1
+    } else {
+      return 1
+    }
+  }
+
+  if (condition === "score"){
+    return b.rt_score - a.rt_score 
+  }
+
+  if (condition === "date"){
+    return b.release_date - a.release_date 
+  }
+})
 
 
 //ordenar alfabetico 
