f081056e16f3230a6fd118ab18dbbd7d68da2452
lyandrasaito <lyandrasaito@gmail.com>
2021-07-06 03:57:24 -0300

    feature/lya#2

--- src/data.js
+++ src/data.js
@@ -1,14 +1,28 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+//export const example = () => {
+//  return 'example';
+//};
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+//export const anotherExample = () => {
+//  return 'OMG';
+//};
 
 //Funções
 
 
+
 import data from './data/ghibli/ghibli.js'
+
+/*
+export function arrayToObject(array){
+    let result = {}
+    for (const element of array){
+        result[element[0]] = element [1];
+    }
+    return result;
+}*/
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,11 +1,28 @@
-import { example } from './data.js';
+import './data.js';
 // import data from './data/lol/lol.js';
 //import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 import data from './data/ghibli/ghibli.js';
+const ghibliMovies = data.films;
 
-console.log(example, data);
-
+//console.log(example, data);
 
 //DOM
+const card = document.getElementById("cards");
+const movie = (movies) => {
+	movies.forEach(ghibliMovie => {
+		card.innerHTML += `
+		<h2>${ghibliMovie.title}</h2>
+		<img src=${ghibliMovie.poster}>
+		<h4>Description: ${ghibliMovie.description}</h4>
+		<h4>Director: ${ghibliMovie.director}</h4>
+		<h4>Producer: ${ghibliMovie.producer}</h4>
+		<h4>Release Date: ${ghibliMovie.release_date}</h4>
+		<h4>Score: ${ghibliMovie.rt_score}</h4>
+		`
+	})
+}
+movie(ghibliMovies);
+
+
