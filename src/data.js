08b3f24ae7a39325436fbec89ccf7a7d5d399df1
marirr86 <rr.mari@hotmail.com>
2021-07-07 09:41:02 -0300

    atualizacao 3

--- src/data.js
+++ src/data.js
@@ -7,3 +7,23 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
+
+export const dataRM = {
+  
+  //FUNÇÃO FILTRAR
+
+  filterData:
+    function (data, condition) {
+      return data.filter(condition);
+    },
+
+  //FUNÇÃO ORDENAR CRESCENTE
+  
+  sortData:
+    function (data, sortBy, sortOrder) {
+      sortOrder == "crescente" ?
+        data.sort((a, b) => { return a[sortBy] > b[sortBy] ? 1 : a[sortBy] < b[sortBy] ? -1 : 0 }) :
+        data.sort((a, b) => { return a[sortBy] > b[sortBy] ? -1 : a[sortBy] < b[sortBy] ? 1 : 0 })
+    },
+
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import { dataRM } from './data.js';
 
-console.log(example, data);
+import data from './data/rickandmorty/rickandmorty.js';
+
+let personagem = Object.values(data.data);
+let filtroAdjetivos = personagem;
+
+//let initialData, filteredData = data.results;
+
+function showcards(cartoesFiltrados) {
+    let cartoes;
+    if (cartoesFiltrados != null) {
+      cartoes = cartoesFiltrados;
+    } else {
+      cartoes = personagem;
+    }
+  
+let i;
+let characterImg;
+let characterName;
+let characterStatus;
+let characterSpecies;
+let characterOrigin;
+
+document.getElementById("cardContainer").innerHTML = "";
