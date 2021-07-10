f9a603d620848e433bd49bf9519031e228a99fa4
Juliete Andrade <julie_sp1990@hotmail.com>
2021-07-09 21:23:10 -0300

    filmes printados na tela

--- src/main.js
+++ src/main.js
@@ -1,6 +1,76 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/ghibli/ghibli.js';
 
-console.log(example, data);
+const films = data.films
+const listFilms = document.getElementById("Films")
+let exibitionFilms = (cards) => {
+     cards.forEach(film => {
+    listFilms.innerHTML += 
+
+    `<li class= "exibitionFilms">
+    <img class="card-poster" src="${film.poster}">
+    <p class= "card-titulo"> title:${film.title}</p>
+    <p class= "card-descrição"> description:${film.description}</p>
+    <p class= "card-diretor"> director:${film.director}</p>
+    <p class= "card-produtor"> producer:${film.producer}</p>
+    <p class= "card-date"> release date:${film.release_date}</p>
+    <p class= "card-score"> rtscore:${film.rt_score}</p>
+</li>`}
+
+     )
+}
+exibitionFilms(films)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
