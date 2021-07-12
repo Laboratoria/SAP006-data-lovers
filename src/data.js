c04d6fa471867ca1e2118053997616c5dc456773
jenniferpessoa <jenni.pessoa@gmail.com>
2021-07-12 14:54:46 -0300

    Filter Name/Order

--- src/data.js
+++ src/data.js
@@ -1,11 +1,35 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+export const filterNames = (Array, searchName) => {
+  if (searchName == "") {
+    throw TypeError("Valor de filtro vazio")
+
+  } else {
+    let filteredNames = Array.filter(champion => champion.id.toUpperCase().includes(searchName))
+    return filteredNames;
+  }
 };
 
-export const teste = () => {
- 
+
+
+export const difficultyOrder = (Array, chosendifficulty) => {
+  let order = Array.sort((a, b) => (a.info.difficulty - b.info.difficulty))
+
+  if (chosendifficulty == 'easy') {
+    let filteredNames = order.filter(champion => champion.info.difficulty <= 4)
+    return filteredNames
+
+  } else if (chosendifficulty == 'medium') {
+    let filteredNames = order.filter(champion => champion.info.difficulty > 4 && champion.info.difficulty <= 7)
+    return filteredNames
+
+  } else if (chosendifficulty == 'hard')  {
+    let filteredNames = order.filter(champion => champion.info.difficulty > 7)
+    return filteredNames
+
+  } else if (chosendifficulty == "") {
+      throw TypeError("Valor de filtro vazio")
+  }
 };
 
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import data from "./data/lol/lol.js";
+import { filterNames } from "./data.js";
+import { difficultyOrder } from "./data.js";
+
 
 // Campeão aparece na tela
-let dataLol = data.data1;
+let dataLol = Object.values(data.data1);
 
 listingCards(dataLol);
 
@@ -10,14 +13,14 @@ function listingCards(itens) {
   cardPack.innerHTML = "";
 
   for (let champion in itens) {
-    const info = dataLol[champion];
-    
+    const info = itens[champion]; //veja que aqui eu alterei o caminho para acessar a propriedade
+        
     const card = document.createElement("li");
     card.innerHTML = `
       <img src="${info.splash}" alt="Imagem do Campeão"/>
       <h3>${info.id}</h3>
        `       
-   cardPack.appendChild(card); 
+    cardPack.appendChild(card); 
 
       // POP-UP //
     const popup = document.querySelector(".popup-wrapper");
@@ -68,17 +71,34 @@ function listingCards(itens) {
   }
 }
 
-// // INFORMAÇÕES DENTRO DO POP-UP //
+//FILTRAR POR NOME 
+let searchName = document.querySelector(".search")
 
-// const popUpContent = document.getElementsByClassName("popup-content");
+searchName.addEventListener('input', event => {
+  searchName = event.target.value.trim().toUpperCase()
+  let dataFilterName = filterNames(dataLol, searchName)
+  console.log(dataFilterName)
+  listingCards(dataFilterName)
+})
 
-//   function showChampionInfo () {
-//     popUpContent.innerHTML += `
-//     <img src="${info.splash}">
-//     <h2 class="champion-name">${info.id}</h2>
-//     <h3 class="champion-title">${info.title}</h3>
-//     <p class="champion-data">${info.info}</p>
-//     <div class="introduction">${info.blurb}</div>
-//     <div class"tags">${info.tags}</div>
-//     `
-//   } console.log(showChampionInfo);
+
+//FILTRAR POR  DIFICULDADE
+/*const categories = document.querySelector(".categorias")
+categories.addEventListener("click", (event) => {
+  const chosendifficulty = event.target.textContent   
+  console.log (chosendifficulty)
+})*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
