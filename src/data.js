95b15f58d974df2085e277dee3c87aad99a67cbc
Carmen Emilia Gozza <carmemilya1@gmail.com>
2021-07-12 16:07:45 -0300

    testes unitarios/botao home

--- src/data.js
+++ src/data.js
@@ -1,24 +1,21 @@
-import data from './data/rickandmorty/rickandmorty.js';
 
-export const getStatus = (statusSelected) => {
+export const getStatus = (data, statusSelected) => {
 
-  let getJson = data.results;
-  getJson = getJson.filter(character => character.status === statusSelected);
-  return getJson; 
+  const getJson = data.filter(character => character.status === statusSelected);
+  return getJson;
+   
 }
 
-export const getSpecies = (specieSelected) => {
+export const getSpecies = (data, specieSelected) => {
 
-  let getJson = data.results;
-  getJson = getJson.filter(character => character.species === specieSelected)
+  const getJson = data.filter(character => character.species === specieSelected)
   return getJson
 
 }
 
-export const getOrder= () =>{
+export const getOrder= (data) =>{
 
-  let showOrder = data.results
-  showOrder = showOrder.sort(function(a,z){
+  let showOrder = data.sort(function(a,z){
     if(a.name < z.name) {
       return -1;
     }
@@ -27,12 +24,13 @@ export const getOrder= () =>{
     }return 0;
   })
   return showOrder
+  
 }
 
-export const getDisorder = () => {
+export const getDisorder = (data) => {
 
-  let showDisorder = data.results
-  showDisorder = showDisorder.sort(function(a,z){
+ 
+  let showDisorder = data.sort(function(a,z){
     if(a.name < z.name) {
       return 1;
     }
@@ -41,4 +39,41 @@ export const getDisorder = () => {
     }return 0;
   })
   return showDisorder
+}
+
+export const getCharacter = (data, NameSearch) => {  
+  
+
+  let object = data.filter(character => character.name.toLocaleLowerCase().includes(NameSearch.toLocaleLowerCase()));
+  return object
+  
+}
+
+export const calculeStats = (data, statusStats) => {
+
+  let total = [];
+  let percent = 0;
+
+  for (let status of data) {
+    if (status.status == statusStats) {
+      total.push(status.status);
+      percent = Number(((total.length / data.length) * 100).toFixed(2))
+    }
+  }
+  return percent;
+}
+
+
+export const calculeSpecies = (data, statusSpecies) => {
+
+  let total = [];
+  let percent = [];
+
+  for (let species of data) {
+    if (species.species == statusSpecies) {
+      total.push(species.species);
+      percent = Number(((total.length / data.length) * 100).toFixed(2))
+    }
+  }
+  return percent;
 }
--- src/main.js
+++ src/main.js
@@ -1,72 +1,96 @@
-import { getStatus, getSpecies, getOrder, getDisorder } from './data.js';
+import { getStatus, getSpecies, getOrder, getDisorder, getCharacter, calculeStats, calculeSpecies } from './data.js';
+
 
 import data from './data/rickandmorty/rickandmorty.js';
 
 
+
 function showCards(data) {
     document.getElementById('get-cards').innerHTML = data.map((item) => `
-        <div class="info-cards">
-        <div class="img-cards">
-        <img src="${item.image}">
+    <div class="info-cards">
+        <div class="card-front">
+          <img src="${item.image}">
         </div>
-            <ul class="list">
-                <li>Nome:${item.name}</li>
-                <li>Status:${item.status}</li>
-                <li>Espécie:${item.species}</li>
-                <li>Origem:${item.origin.name}</li>
-            </ul>
+        <div class="card-back">
+          <ul class="list">
+            <li id="name">Nome:${item.name}</li>
+            <li class= "list-item"> Status:${item.status}</li>
+            <li class= "list-item">Espécie:${item.species}</li>
+            <li class= "list-item">Origem:${item.origin.name}</li>
+          </ul>
         </div>
+    </div>
         
         `)
 }
+
 showCards(data.results);
 
-const btnSearch = document.getElementById("button-search");
-
-function searchCharacter(e) {
-    e.preventDefault()
-
-    let searchTerm = document.getElementById("characters").value;
-    console.log(searchTerm);
-    let object = data.results;
-    object = object.filter(character => character.name === searchTerm);
-    showCards(object);
-}
-
-btnSearch.addEventListener('click', searchCharacter);
-
-
+const searchTerm = document.getElementById("characters");
 const status = document.getElementById("status");
 const species= document.getElementById("especie");
 const order = document.getElementById("ordenar");
 const disorder = document.getElementById("desordenar");
+const showStats = document.getElementById("showStats")
+const homeButton = document.getElementById("home");
 
+
+function searchCharacter() {
+    
+    showCards(getCharacter(data.results, searchTerm.value));
+    
+}
+
+searchTerm.addEventListener('keyup', () => {searchCharacter() } );
+
+
+
+function calculoAgregadoStatus() {
+
+    const stats = calculeStats(data.results, status.value);
+    showStats.innerHTML = (`A porcentagem de personagens dessa categoria é de: ${stats} %`);
+    
+}
 
 function getStatusData() {
+                     
+    showCards(getStatus(data.results, status.value));
     
-    showCards(getStatus(status.value));
 }
-status.addEventListener("change", () => {getStatusData()} );
+status.addEventListener("change", () => {getStatusData(), calculoAgregadoStatus()} );
 
+function calculoAgregadoSpecies() {
+
+    const newSpecies = calculeSpecies(data.results, species.value);
+    showStats.innerHTML = (`A porcentagem de personagens dessa categoria é de: ${newSpecies} %`);
+    
+}
 
 function getSpeciesData(){
 
-    showCards(getSpecies(species.value));
+    showCards(getSpecies(data.results, species.value));
+    
 }
-species.addEventListener("change", () => {getSpeciesData()} );
+species.addEventListener("change", () => {getSpeciesData(), calculoAgregadoSpecies()} );
 
 
 function getOrderData(){
 
-    showCards(getOrder(order.value));
+    showCards(getOrder(data.results));
 }
 order.addEventListener("click", () => {getOrderData()} );
 
 
 function getDisorderData() {
 
-    showCards(getDisorder(disorder.value));
+    showCards(getDisorder(data.results));
 }
 disorder.addEventListener("click", () => {getDisorderData()} );
+ 
+function clear(){
+    
+    showStats.innerHTML = "";
+    showCards(data.results)
+}
 
-
+homeButton.addEventListener("click", clear);
