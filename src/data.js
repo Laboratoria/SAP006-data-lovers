ead093a1eb046eb58eece6ad6538fb96ca49e284
belobruna <bruna.belo@gmail.com>
2021-07-12 13:48:00 -0300

    cards personagens

--- src/data.js
+++ src/data.js
@@ -7,23 +7,19 @@
 // };
 
 
- export function showFilms(film) {
-   film.map((name) => console.log(name.title))
-  }
+export function showFilms(film) {
+  film.map((name) => console.log(name.title))
+};
 
-  export function showCharacters(character) {
-    character.map((name) => console.log(name.people))
-   }
-   
-// export function showCharacters(film) {
-// film.map((name) => console.log(name.title))
-// }
+
+
+
 
 // filterData(data, condition): esta função receberia os dados e nos retornaria os que cumprem com a condição.
 
 // sortData(data, sortBy, sortOrder): esta função recebe três parâmetros. O primeiro, data, nos entrega os dados. 
-  //O segundo, sortBy, diz respeito a qual das informações quer usar para ordenar. O terceiro, sortOrder, 
-  //indica se quer ordenar de maneira crescente ou decrescente.
+ //O segundo, sortBy, diz respeito a qual das informações quer usar para ordenar. O terceiro, sortOrder, 
+ //indica se quer ordenar de maneira crescente ou decrescente.
 
 // computeStats(data): essa função nos permite fazer cálculos estatísticos básicos para serem exibidos de acordo 
-  //com o que os dados permitem.
+ //com o que os dados permitem.
--- src/menu-characters.js
+++ src/menu-characters.js
@@ -1,27 +1,78 @@
 import { showFilms } from './data.js';
 import data from './data/ghibli/ghibli.js';
 
-console.log(showFilms(data.films['people']))
+// console.log(data.films[0].people[0].name)
+// console.log(data.films[1].people[1].name)
 
-//função para criar cards dos filmes
-//const listaFilmes = showFilms(data.ghibli)
+const movies = data.films
 
 
-const printCardsCharacters = (characterList) => {
-    characterList.forEach(character => {
+function getPeople() {
+    let people = [];
+    for (let film of movies){
+        for (let char of film.people)
+        people.push(char)
+    }
+    return people
+}
+
+
+const printCardsFilms = (listaPersonagens) => {
+    listaPersonagens.map(people => {
         const characterCardsElements = document.getElementById("boxCharacaterCards");
         characterCardsElements.innerHTML += `
-        <div class="filmCard">
+        <div class="characterCard">
             <div class="flipper">
                 <div class="frontCard"> 
-                    <img class="peopleImg" src= ${character.img}>
+                    <img class="characterPoster" src= ${people.img}>
                 </div>
                 <div class="backCard">
-                    <h2 class="peopleName"> ${character.name}</h2> 
+                    <h3 class="character-name"> ${people.name}</h3> <br>
+                    <p> <b>Gender:</b> ${people.gender} </p>
+                    <p> <b>Age:</b> ${people.age} </p>
+                    <p> <b>Specie:</b> ${people.specie} </p>
                 </div>
             </div>
         </div>`
     })
 }
 
-printCardsCharacters(data.people)
+printCardsFilms(getPeople())
+
+
+
+/* const people = data.films[0].people
+console.log(people)
+
+people.map(showPersonagens)
+
+function showPersonagens(item){
+    console.log(item['name'], item['img'])
+}   
+
+
+
+// console.log(showFilms(data.films['people']))
+
+// //função para criar cards dos filmes
+// //const listaFilmes = showFilms(data.ghibli)
+
+
+// const printCardsCharacters = (characterList) => {
+//     characterList.forEach(character => {
+//         const characterCardsElements = document.getElementById("boxCharacaterCards");
+//         characterCardsElements.innerHTML += `
+//         <div class="filmCard">
+//             <div class="flipper">
+//                 <div class="frontCard"> 
+//                     <img class="peopleImg" src= ${character.img}>
+//                 </div>
+//                 <div class="backCard">
+//                     <h2 class="peopleName"> ${character.name}</h2> 
+//                 </div>
+//             </div>
+//         </div>`
+//     })
+// }
+
+// printCardsCharacters(data.people)*/
