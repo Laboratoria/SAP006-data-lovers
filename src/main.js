9354b971f19f8e8ffaa2e8cc7c67e94ad78d02d1
jenniferpessoa <jenni.pessoa@gmail.com>
2021-07-14 14:14:55 -0300

    Gráfico

--- src/main.js
+++ src/main.js
@@ -15,19 +15,19 @@ function listingCards(itens) {
 
   for (let champion in itens) {
     const info = itens[champion];
-    
+
     const card = document.createElement("li");
     card.innerHTML = `
       <img src="${info.splash}" alt="Imagem do Campeão"/>
       <h3>${info.id}</h3>
-       `       
-    cardPack.appendChild(card); 
+       `
+    cardPack.appendChild(card);
 
-    
-      // POP-UP //
+
+    // POP-UP //
     const popup = document.querySelector(".popup-wrapper");
     // CONTEUDO DO POP-UP //
-    const popUpContent = document.querySelector(".popup-content");    
+    const popUpContent = document.querySelector(".popup-content");
 
     card.addEventListener("click", () => {
       popup.style.display = "block";
@@ -65,66 +65,69 @@ function listingCards(itens) {
 
         </div>
       `;
-    
-    //RELAÇÃO COM O HTML
-    const grafic = document.querySelector("#barchart_material")
-    grafic.innerHTML = `
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>`
 
-    //CHAMAMENTO DA FUNÇÃO DRAW-CHART
-    google.charts.load('current', {'packages':['bar']});
-    google.charts.setOnLoadCallback(drawChart);    
-    
-    //CRIAÇÃO DO GRÁFICO E EDIÇÃO 
-    function drawChart() {
-      let tagChampion = `${info.tags[0]}`//AQUI É O NOME DA CATEGORIA À QUAL O CAMPEÃO PERTENCE 
-      let tagSelection = filterByTag(dataLol, tagChampion ) //AQUI É ONDE A GENTE CHAMA A SUA FUNÇÃO DE SELECIONAR POR CATEGORIA 
+      /*const habChampion = Object.values(info.info) 
+      console.log(habChampion)*/
 
-                  //CADA VARIÁVEL CORRESPONDE A UMA MÉDIA POR HABILIDADE: ATAQUE, DEFESA ETC. 
-                  //PRECISAREMOS CRIAR UMA FUNÇÃO 
-      let medAtt = Math.round((tagSelection.reduce((prevCard, nextCard) => prevCard + nextCard.info.attack, 0))/tagSelection.length)
-      let medDef = Math.round((tagSelection.reduce((prevCard, nextCard) => prevCard + nextCard.info.defense, 0))/tagSelection.length)
-      let medMag = Math.round((tagSelection.reduce((prevCard, nextCard) => prevCard + nextCard.info.magic, 0))/tagSelection.length)
-      let medDiff = Math.round((tagSelection.reduce((prevCard, nextCard) => prevCard + nextCard.info.difficulty, 0))/tagSelection.length)
+      //RELAÇÃO COM O HTML
+      const grafic = document.querySelector("#barchart_material")
+      grafic.innerHTML = `
+      <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>`
 
-                  // PREENCHIMENTO DAQUELAS POSIÇÕES DO ARRAY, COMO SE FOSSE UMA TABELINHA
-      let data = google.visualization.arrayToDataTable([
-        ['HABILIDADES', `${info.id}`, `${tagChampion}`, 'Máximo'],
-        ['ATAQUE', info.info.attack, medAtt, 10],
-        ['DEFESA', info.info.defense, medDef, 10],
-        ['MAGIA', info.info.magic, medMag, 10],
-        ['DIFICULDADE', info.info.difficulty, medDiff, 10]
-      ]);   
+      //CHAMAMENTO DA FUNÇÃO DRAW-CHART
+      google.charts.load('current', { 'packages': ['bar'] });
+      google.charts.setOnLoadCallback(drawChart);
 
-              //AQUI É ONDE COLOCAMOS AS PROPRIEDADES DO OBJETO QUE SERVEM PARA EDITAR O GRÁFICO 
-      let options = {
-        width: 800,
-        height: 400,
-        backgroundColor: {
-          fill:'#171820'     
-        },       
-           
-        chart: {
-        title: `Habilidades de ${info.id}`, // ESSE É O TÍTULO DO GRÁFICO       
-        //subtitle: 'Sales, Expenses, and Profit: 2014-2017',
-        },
-        chartArea: {
+      //CRIAÇÃO DO GRÁFICO E EDIÇÃO 
+      function drawChart() {
+        let tagChampion = `${info.tags[0]}`//AQUI É O NOME DA CATEGORIA À QUAL O CAMPEÃO PERTENCE 
+        let tagSelection = filterByTag(dataLol, tagChampion) //AQUI É ONDE A GENTE CHAMA A SUA FUNÇÃO DE SELECIONAR POR CATEGORIA 
+
+        //CADA VARIÁVEL CORRESPONDE A UMA MÉDIA POR HABILIDADE: ATAQUE, DEFESA ETC. 
+        //PRECISAREMOS CRIAR UMA FUNÇÃO 
+        let medAtt = Math.round((tagSelection.reduce((prevCard, nextCard) => prevCard + nextCard.info.attack, 0)) / tagSelection.length)
+        let medDef = Math.round((tagSelection.reduce((prevCard, nextCard) => prevCard + nextCard.info.defense, 0)) / tagSelection.length)
+        let medMag = Math.round((tagSelection.reduce((prevCard, nextCard) => prevCard + nextCard.info.magic, 0)) / tagSelection.length)
+        let medDiff = Math.round((tagSelection.reduce((prevCard, nextCard) => prevCard + nextCard.info.difficulty, 0)) / tagSelection.length)
+
+        // PREENCHIMENTO DAQUELAS POSIÇÕES DO ARRAY, COMO SE FOSSE UMA TABELINHA
+        let data = google.visualization.arrayToDataTable([
+          ['HABILIDADES', `${info.id}`, `${tagChampion}`, 'Máximo'],
+          ['ATAQUE', info.info.attack, medAtt, 10],
+          ['DEFESA', info.info.defense, medDef, 10],
+          ['MAGIA', info.info.magic, medMag, 10],
+          ['DIFICULDADE', info.info.difficulty, medDiff, 10]
+        ]);
+
+        //AQUI É ONDE COLOCAMOS AS PROPRIEDADES DO OBJETO QUE SERVEM PARA EDITAR O GRÁFICO 
+        let options = {
+          width: 800,
+          height: 400,
           backgroundColor: {
-            fill: '#171820',
-            fillOpacity: 0.1
+            fill: '#171820'
           },
-        },        
-        bars: 'horizontal' // Required for Material Bar Charts.
-      };
-      
-                //AQUI PEGAMOS A DIV OUTRA VEZ. EU NÃO SEI PQ, MAS SE TIRAR NÃO FUNCIONA KKKK
-      let chart = new google.charts.Bar(document.getElementById('barchart_material'));
 
-      chart.draw(data, google.charts.Bar.convertOptions(options));
-    }
-    });    
-  
-      // FECHAR POP-UP //
+          chart: {
+            title: `Habilidades de ${info.id}`, // ESSE É O TÍTULO DO GRÁFICO       
+            //subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+          },
+          chartArea: {
+            backgroundColor: {
+              fill: '#171820',
+              fillOpacity: 0.1
+            },
+          },
+          bars: 'horizontal' // Required for Material Bar Charts.
+        };
+
+        //AQUI PEGAMOS A DIV OUTRA VEZ. EU NÃO SEI PQ, MAS SE TIRAR NÃO FUNCIONA KKKK
+        let chart = new google.charts.Bar(document.getElementById('barchart_material'));
+
+        chart.draw(data, google.charts.Bar.convertOptions(options));
+      }
+    });
+
+    // FECHAR POP-UP //
     popup.addEventListener("click", (event) => {
       const classNameOfClickedElement = event.target.classList[0];
       const classNames = ["popup-close", "popup-wrapper"];
@@ -134,7 +137,7 @@ function listingCards(itens) {
         popup.style.display = "none";
       }
     });
-  } 
+  }
 }
 
 //FILTRAR POR NOME 
@@ -151,21 +154,21 @@ searchName.addEventListener('input', event => {
 const filterButton = document.querySelector(".categorias");
 filterButton.addEventListener('click', (event) => {
   const botaoClicado = event.target.textContent;
-  
+
   if (botaoClicado == "Todos") {
     listingCards(dataLol)
   } else {
     const filtered = filterByTag(dataLol, botaoClicado);
     listingCards(filtered);
-    }
+  }
 });
 
 
 //FILTRAR POR  DIFICULDADE - NÃO ESTOU CONSEGUINDO FAZER O DOM 
 const categories = document.querySelector(".dropbtn")
 categories.addEventListener("click", (event) => {
-let chosendifficulty = event.target.textContent; 
-  
+  let chosendifficulty = event.target.textContent;
+
   // if (chosendifficulty == "No filter") {
   //   listingCards(dataLol)
   // } else {
@@ -173,8 +176,10 @@ let chosendifficulty = event.target.textContent;
   //   listingCards(filtered);
   // }
 
-  
- })
+
+})
+
+
 
 
 /*onst filterByTag = function (array, botaoClicado){
