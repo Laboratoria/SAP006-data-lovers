1eff6bdd7c372fbdb2594ab6614ce06aae6153c0
Thais <thais_fernandes62@hotmail.com>
2021-07-14 21:18:01 -0300

    main: fetch para pegar arquivo json; data/test: feito condição false {return false} e teste

--- src/main.js
+++ src/main.js
@@ -1,25 +1,35 @@
 import { computeStats, filterData, sortData, searchName } from "./data.js";
-import data from "./data/rickandmorty/rickandmorty.js"
+//import data from "./data/rickandmorty/rickandmorty.js"
 
-const cards = document.querySelector(".cards");
-let genericCards = "";
-const btnClear = document.getElementById("btn-clearFilters");
-const btnSort = document.getElementById("sort-btn");
-const searchInput = document.getElementById("search");
+fetch("./data/rickandmorty/rickandmorty.json")
+  .then(answer => answer.json())
+  .then(info => {
+    let finalData = info
+    setData(finalData)
+  })
 
-const statusFilter = document.getElementById("status-filter");
-const genderFilter = document.getElementById("gender-filter");
+function setData(finalData) {
+  let data = finalData;
 
-const printTotalCharacters = document.getElementById("totalCharacters");
-const printGenderAverage = document.getElementById("genderAverage");
+  const cards = document.querySelector(".cards");
+  let genericCards = "";
+  const btnClear = document.getElementById("btn-clearFilters");
+  const btnSort = document.getElementById("sort-btn");
+  const searchInput = document.getElementById("search");
 
-printCardsGeneric(data.results);
+  const statusFilter = document.getElementById("status-filter");
+  const genderFilter = document.getElementById("gender-filter");
 
-function printCardsGeneric(filterChosen) {
-  genericCards = filterChosen
-    .map(
-      ({ name, status, gender, image, episode }) =>
-        `<div class="cards_container">
+  const printTotalCharacters = document.getElementById("totalCharacters");
+  const printGenderAverage = document.getElementById("genderAverage");
+
+  printCardsGeneric(data.results);
+
+  function printCardsGeneric(filterChosen) {
+    genericCards = filterChosen
+      .map(
+        ({ name, status, gender, image, episode }) =>
+          `<div class="cards_container">
          <div class="character_img">
            <img src="${image}">
          </div>
@@ -32,25 +42,25 @@ function printCardsGeneric(filterChosen) {
              <p>${episode.map((i) => i.replaceAll(/[^0-9]/g, " "))}</p>
           </div>
       </div>`
-    )
-    .join("");
+      )
+      .join("");
 
-  cards.innerHTML = "";
-  cards.innerHTML += genericCards;
-}
+    cards.innerHTML = "";
+    cards.innerHTML += genericCards;
+  }
 
 
-const totalCharacters = computeStats.characters(data.results);
+  const totalCharacters = computeStats.characters(data.results);
 
-printTotalCharacters.innerHTML = `<p class="totalCharacter">O total de personagens da série é:</p>
+  printTotalCharacters.innerHTML = `<p class="totalCharacter">O total de personagens da série é:</p>
                                   <p class="numberOfCharacters">${totalCharacters}</p>`;
 
-const maleAverage = computeStats.gender(data.results, "Male") + "%";
-const femaleAverage = computeStats.gender(data.results, "Female") + "%";
-const genderlessAverage = computeStats.gender(data.results, "Genderless") + "%";
-const unknownAverage = computeStats.gender(data.results, "unknown") + "%";
+  const maleAverage = computeStats.gender(data.results, "Male") + "%";
+  const femaleAverage = computeStats.gender(data.results, "Female") + "%";
+  const genderlessAverage = computeStats.gender(data.results, "Genderless") + "%";
+  const unknownAverage = computeStats.gender(data.results, "unknown") + "%";
 
-printGenderAverage.innerHTML = `<p class="genderAverage">Média de gêneros:</p>
+  printGenderAverage.innerHTML = `<p class="genderAverage">Média de gêneros:</p>
                                 <p class="genderAverage">Masculinos:
                                   <span>${maleAverage}</span>
                                 </p>
@@ -64,33 +74,35 @@ printGenderAverage.innerHTML = `<p class="genderAverage">Média de gêneros:</p>
                                   <span>${genderlessAverage}</span>
                                 </p>`;
 
-function filter(e) {
-  e.preventDefault();
-  const statusOptions = statusFilter.options[statusFilter.selectedIndex].value;
-  const genderOptions = genderFilter.options[genderFilter.selectedIndex].value;
-  const filterValue = filterData(data.results, statusOptions, genderOptions);
-  printCardsGeneric(filterValue);
-}
-statusFilter.addEventListener("click", filter);
-genderFilter.addEventListener("click", filter);
+  function filter(e) {
+    e.preventDefault();
+    const statusOptions = statusFilter.options[statusFilter.selectedIndex].value;
+    const genderOptions = genderFilter.options[genderFilter.selectedIndex].value;
+    const filterValue = filterData(data.results, statusOptions, genderOptions);
+    printCardsGeneric(filterValue);
+  }
+  statusFilter.addEventListener("change", filter);
+  genderFilter.addEventListener("change", filter);
 
-function sort(e) {
-  e.preventDefault();
-  const sortCards = sortData(data.results);
-  printCardsGeneric(sortCards);
-}
-btnSort.addEventListener("click", sort);
+  function sort(e) {
+    e.preventDefault();
+    const sortCards = sortData(data.results);
+    printCardsGeneric(sortCards);
+  }
+  btnSort.addEventListener("click", sort);
 
-function clearFilters(e) {
-  e.preventDefault();
-  printCardsGeneric(data.results);
-  statusFilter.options[(statusFilter.selectedIndex = 0)];
-  genderFilter.options[(genderFilter.selectedIndex = 0)];
-}
-btnClear.addEventListener("click", clearFilters);
+  function clearFilters(e) {
+    e.preventDefault();
+    printCardsGeneric(data.results);
+    statusFilter.options[(statusFilter.selectedIndex = 0)];
+    genderFilter.options[(genderFilter.selectedIndex = 0)];
+  }
+  btnClear.addEventListener("click", clearFilters);
 
-function searchByName(e) {
-  const charactersByName = searchName(data.results, e.target.value);
-  printCardsGeneric(charactersByName);
+  function searchByName(e) {
+    const charactersByName = searchName(data.results, e.target.value);
+    printCardsGeneric(charactersByName);
+  }
+  searchInput.addEventListener("keyup", searchByName);
+
 }
-searchInput.addEventListener("keyup", searchByName);
