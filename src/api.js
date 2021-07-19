195210cbcd42e7f88f5c01da4059fdc883bf240a
Mariana Andrade <andrade.marianaferreira@gmail.com>
2021-07-19 17:27:23 -0300

    incluindo o mock dos testes

---
+++ src/api.js
@@ -0,0 +1,28 @@
+export const mockPersonagens = [
+    {
+        "name": "Beth Smith",
+        "status": "Alive",
+        "species": "Human",
+        "gender": "Female",
+    },
+    {
+        "name": "Morty Smith",
+        "status": "Alive",
+        "species": "Human",
+        "gender": "Male",
+    },
+    {
+        "name": "Rick Sanchez",
+        "status": "Alive",
+        "species": "Human",
+        "gender": "Male",
+    },
+    {
+        "name": "Summer Smith",
+        "status": "Alive",
+        "species": "Human",
+        "gender": "Female",
+    },
+]
+
+export const [ Beth, Morty, Rick, Summer ] = mockPersonagens;
