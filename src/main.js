c4fdb1141997431fe8c915aa9f0feef9a5c96e30
Beatriz Ferraz <beaferraz2010@gmail.com>
2021-07-19 18:05:48 -0300

    Atualizações

--- src/main.js
+++ src/main.js
@@ -6,28 +6,49 @@
 //const primeirofilme[]= ghibli.films[0]
 //console.log(primeirofilme);
 
-import data from './data/ghibli/ghibli.js';
+import data from "./data/ghibli/ghibli.js";
 
 /////////////////// Importar Filmes ////////////////////
 
-const films = data.films
-
+const films = data.films;
 const listFilms = document.getElementById("Films")
-let exibitionFilms = (cards) => { 
+let exibitionFilms = (cards) => {
     cards.forEach(film => {
     listFilms.innerHTML +=
- 
+
     `<li class="exibitionFilms"> 
-    <img class="card-poster" src=" ${film.poster}">
-    <p class="card-titulo"> Title: ${film.title} </p> 
-    <p class="card-lançamento"> Release date: ${film.release_date} </p>
-    <p class="card-descrição"> Description: ${film.description} </p>    
-    <p class="card-diretor"> Director: ${film.director} </p>
-    <p class="card-producer"> Producer: ${film.producer} </p>
-    <p class="card-avaliação"> Score: ${film.rt_score} </p>
-</li>`}
+    <img class="card-poster" src="${film.poster}">
+    <p class="card-titulo"> title:${film.title}</p> 
+    <p class="card-descrição"> description:${film.description}</p>
+    <p class="card-diretor"> director:${film.director}</p>
+    <p class="card-produtor"> Producer:${film.producer}</p> 
+    <p class="card-date"> Release date:${film.release_date}</p>
+    <p class="card-score"> rtscore:${film.rt_score}</p>
+ </li>`}
 
-    )
-
+      )
 }
 exibitionFilms(films)
+
+//Personagens
+
+ const charactersList = document.getElementById("Characters")
+ const exibitionPeople = (cards) => {
+   cards.forEach(people => {
+         charactersList.innerHTML +=
+
+    `<li class="cardCharacter">
+           <img src=${people.img} class="card-img" alt="ImagemDoPersonagem">
+          <p class="card-name"> character:${people.name}<p>
+          <p class="card-gender"> Gender:${people.gender}</p>
+         <p class="card-age"> Age:${people.age}</p>
+         <p class="card-specie"> Specie: ${people.specie}</p>
+     </li> `}
+
+     )
+ }
+
+for(let film of films){
+      let charactersList = film.people
+      exibitionPeople(charactersList , film.title)
+ }
