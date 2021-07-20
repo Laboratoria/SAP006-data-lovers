eddb123e83a95e2a48da9d4ea9736cfe129e44d2
jenniferpessoa <jenni.pessoa@gmail.com>
2021-07-20 10:17:41 -0300

    Test, Pop-up  e Cálculo Agregado

--- src/data.js
+++ src/data.js
@@ -1,61 +1,35 @@
 // estas funciones son de ejemplo
 
-export const filterNames = (Array, searchName) => {
-  if (searchName == "") {
-    throw TypeError("Valor de filtro vazio")
-
-  } else {
-    let filteredNames = Array.filter(champion => champion.id.toUpperCase().includes(searchName));
-    return filteredNames;
-  }
+export const filterNames = (Array, string) => {
+    return Array.filter(champion => champion.id.toUpperCase().includes(string));
 };
 
 export const filterByTag = function (array, string){ //mudei para string assim, poderíamos usar  qualquer variável como argumento
-  const championTags = array.filter(champion => champion.tags[0] == string || champion.tags[1] === string);
-  return championTags;
+  return array.filter(champion => champion.tags[0] == string || champion.tags[1] === string)
 };
 
 
 export const difficultyOrder = (array, chosendifficulty) => {  
-  let dataLol2 = array.slice(0, array.length)
-  let order = dataLol2.sort((a, b) => (a.info.difficulty - b.info.difficulty));
-  let filteredNames = []
-
+  const order = (array.slice(0, array.length)).sort((a, b) => (a.info.difficulty - b.info.difficulty))
+    
   switch (chosendifficulty){
     case '1':
-      return array
-      break
-
+      return order
+      
     case '2':
-      filteredNames = order.filter(champion => champion.info.difficulty <= 4);
-      return filteredNames;
-      break
-    
+      return order.filter(champion => champion.info.difficulty <= 4)
+      
     case '3':
-      filteredNames = order.filter(champion => champion.info.difficulty > 4 && champion.info.difficulty <= 7)
-      return filteredNames;
-      break
-      
+      return order.filter(champion => champion.info.difficulty > 4 && champion.info.difficulty <= 7)
+          
     case '4': 
-      filteredNames = order.filter(champion => champion.info.difficulty > 7);
-      return filteredNames;
-        break
-      
-      default: 
-        window.alert(`Sorry, chose your champion.`)      
+      return order.filter(champion => champion.info.difficulty > 7)
+           
+    default: 
+    window.alert(`Sorry, chose your champion.`)      
   }
-};
+}
 
-/*
-  if (chosendifficulty === '2') {
-    let filteredNames = order.filter(champion => champion.info.difficulty <= 4);
-    return filteredNames;
-
-  } else if (chosendifficulty === '3') {
-    let filteredNames = order.filter(champion => champion.info.difficulty > 4 && champion.info.difficulty <= 7)
-    return filteredNames;
-
-  } else if (chosendifficulty === '4')  {
-    let filteredNames = order.filter(champion => champion.info.difficulty > 7);
-    return filteredNames;
-  }*/
+export const mediaInfo = function (array, key){ 
+  return  Math.round((array.reduce((accum, champion) => accum + champion.info[key], 0)) / array.length)
+}
